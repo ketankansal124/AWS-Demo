e3b75c61a7214ceb4f0be0b3a818d40d767e41e8
KetanKansal <ketankansal124@gmail.com>
2025-03-17 07:54:00 +0530

    Code Formatted

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express=require("express");
-const ip=require("ip");
+const express = require("express");
+const ip = require("ip");
 
 
-const app =express();
+const app = express();
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     return res.json({
-        message:"pong",
-        serverAddress:ip.address()
+        message: "pong",
+        serverAddress: ip.address()
     })
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server is running on port 3000")
 });
